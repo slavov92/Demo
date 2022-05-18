d2a2584dbcccc02f80f6cf16fcaa0cffab31c765
slavov92 <slavov92@gmail.com>
2022-05-18 14:28:29 +0300

    pinetree

--- src/Demo/PineTree.java
+++ src/Demo/PineTree.java
@@ -1,14 +1,22 @@
 package Demo;
 
+import java.util.Scanner;
+
 public class PineTree {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How big is your tree?");
+        int x = scanner.nextInt();
+        scanner.close();
 
-        System.out.println("This is not a tree!");
-        System.out.println("BMW is not a tree");
-
-
-        System.out.println("ASD");
+        for (int i = 0; i < x; i++){
+            for (int j = 0; j < x - i; j++)
+                System.out.print(" ");
+            for (int k=0; k < (2* i + 1); k++)
+                System.out.print("*");
+            System.out.println();
+        }
 
 
 
