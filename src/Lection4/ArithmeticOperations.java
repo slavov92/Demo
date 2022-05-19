55f1e5e93f5c44bd7ea017e42cee4ee72f34dbea
slavov92 <slavov92@gmail.com>
2022-05-19 19:11:54 +0300

    new commit

---
+++ src/Lection4/ArithmeticOperations.java
@@ -0,0 +1,29 @@
+package Lection4;
+
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+
+      int num1 = 10;
+      int num2 = 50;
+
+      num2= num1;
+
+
+        num2=num1;System.out.println("= Result is: " + num2);
+        num2+=num1;System.out.println("+ Result is: " + num2);
+        num2-=num1;System.out.println("- Result is: " + num2);
+        num2*=num1;System.out.println("* Result is: " + num2);
+        num2/=num1;System.out.println("/ Result is: " + num2);
+        num2%=num1;System.out.println("% Result is: " + num2);
+
+        //num1++; System.out.println("++ Result is " + num1);
+        num1--; System.out.println("-- Result is " + num1);
+        num1--; System.out.println("-- Result is " + num1);
+
+
+    }
+
+
+
+}
---
+++ src/Lection4/Operators.java
@@ -0,0 +1,17 @@
+package Lection4;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        boolean b1 = true;
+
+        boolean b2 = false;
+
+        System.out.println("b1 && b2: " + (b1&&b2));
+
+        System.out.println("b1 || b2: " + (b1||b2));
+
+        System.out.println("!(b1 && b2): " + !(b1&&b2));
+    }
+}
