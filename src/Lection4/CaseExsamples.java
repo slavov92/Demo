f5b46ae56350d67a8cd8f08aa3b83bf3b9a8719d
slavov92 <slavov92@gmail.com>
2022-05-24 16:16:36 +0300

    lection4

---
+++ src/Lection4/CaseExsamples.java
@@ -0,0 +1,41 @@
+package Lection4;
+
+public class CaseExsamples {
+
+    public static void main(String[] args) {
+
+        int i=90;
+
+        switch(i)
+
+        {
+
+            case 1:
+
+                System.out.println("Case1 ");
+
+                break;
+
+            case 2:
+
+                System.out.println("Case2 ");
+
+                break;
+
+            case 3:
+
+                System.out.println("Case3 ");
+
+                break;
+            case 90:
+                System.out.println("Triangle is right-angled");
+                break;
+
+            default:
+
+                System.out.println("Default ");
+
+        }
+
+    }
+}
---
+++ src/Lection4/TriangleTypes.java
@@ -0,0 +1,73 @@
+package Lection4;
+import java.util.Scanner;
+public class TriangleTypes {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter angle A of triangle: ");
+        Double angleA = scanner.nextDouble();
+
+        System.out.println("Enter angle B of triangle: ");
+        Double angleB = scanner.nextDouble();
+
+        System.out.println("Enter angle C of triangle: ");
+        Double angleC = scanner.nextDouble();
+
+        double result = angleA + angleB + angleC;
+
+        if (angleA > 0 && angleB > 0 && angleC > 0) {
+
+            if (result == 180) {
+                System.out.println("Angles are with correct values!");
+
+                if (angleA == 90 || angleB == 90 || angleC == 90) {
+                    System.out.println("The triangle is right-angled (правоъгълен)!");
+                    if ((angleA.equals(angleB)) || (angleA.equals(angleC)) || (angleB.equals(angleC))){
+                        System.out.println("The triangle are isosceles (равнобедрен)");
+                    }
+                } else if (angleA < 90 && angleB < 90 && angleC < 90) {
+
+                    if (angleA == 60 && angleB == 60 && angleC == 60) {
+                        System.out.println("Triangle is equilateral (равностранен) !");
+                        System.out.println("The triangle is acute (остроъгълен)!");
+                    }
+                    else  {
+                        System.out.println("The triangle is acute (остроъгълен)!");
+
+                        if ((angleA.equals(angleB)) || (angleA.equals(angleC)) || (angleB.equals(angleC))){
+                            System.out.println("The triangle is равнобедрен!");
+                        }   else if (angleA != angleB || angleA!=angleC || angleB !=angleC){
+                            System.out.println("");
+                        }
+                    }
+
+                   // System.out.println("The triangle is acute(остроъгълен)!");
+                } else if (angleA > 90 || angleB > 90 || angleC > 90){
+
+                    if ((angleA.equals(angleB)) || (angleA.equals(angleC)) || (angleB.equals(angleC))){
+                        // не става със "=="
+                        System.out.println("The triangle is 'obtuse'(тъпоъгълен)!");
+                        System.out.println("The triangle is равнобедрен!");
+                    }
+                    else {
+                        System.out.println("The triangle is 'obtuse'(тъпоъгълен)!");
+                    }
+
+                }
+
+
+            } else if (result > 180) {
+                System.out.println("Sum of angles is more than 180 degree, include new values!");
+            } else if (result < 180) {
+                System.out.println("Sum of angles is less than 180 degree, include new values!");
+            }
+
+
+        }
+        else {
+
+            System.out.println("Include positive values!");
+        }
+    }
+}
